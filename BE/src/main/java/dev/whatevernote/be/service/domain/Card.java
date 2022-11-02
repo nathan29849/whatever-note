068ae158a1fd5b71060c2a07036b9d08c22bc163
nathan29849 <phs960418@gmail.com>
2022-11-02 16:48:11 +0900

    feat: [#22] 도메인 엔티티 추가 (Card, Content)

---
+++ BE/src/main/java/dev/whatevernote/be/service/domain/Card.java
@@ -0,0 +1,33 @@
+package dev.whatevernote.be.service.domain;
+
+import dev.whatevernote.be.common.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@SQLDelete(sql = "UPDATE note SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
+@Entity
+public class Card extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+
+	protected Card() {}
+
+	@Column(name="card_order")
+	private Long seq;
+
+	private String title;
+
+	@ManyToOne
+	@JoinColumn(name="note_id")
+	private Note note;
+}
