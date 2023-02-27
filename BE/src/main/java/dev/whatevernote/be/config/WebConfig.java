bc0f555926ee47d5898eafaacc95c88510de7d45
nathan29849 <phs960418@gmail.com>
2023-02-27 23:18:19 +0900

    feat: [#74] 로그인 초기 작업 세팅 (Config, build.gradle 등)

--- BE/src/main/java/dev/whatevernote/be/config/WebConfig.java
+++ BE/src/main/java/dev/whatevernote/be/config/WebConfig.java
@@ -1,11 +1,24 @@
 package dev.whatevernote.be.config;
 
+import dev.whatevernote.be.login.AuthInterceptor;
+import dev.whatevernote.be.login.LoginArgumentResolver;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+	private final AuthInterceptor authInterceptor;
+	private final LoginArgumentResolver loginArgumentResolver;
+
+	public WebConfig(AuthInterceptor authInterceptor, LoginArgumentResolver loginArgumentResolver) {
+		this.authInterceptor = authInterceptor;
+		this.loginArgumentResolver = loginArgumentResolver;
+	}
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -14,4 +27,19 @@ public class WebConfig implements WebMvcConfigurer {
 			.allowedOrigins("*");
 	}
 
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor)
+			.addPathPatterns("/**")
+			.excludePathPatterns(
+				"/error/**",
+				"/login/kakao/**",
+				"/docs/**"
+			);
+	}
+
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(loginArgumentResolver);
+	}
 }
