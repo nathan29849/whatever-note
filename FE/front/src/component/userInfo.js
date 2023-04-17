6db7813a9b735fe0f8e9cdbe27ca5045b36f16b3
onlinekkabchi <onlinekkabchi@gmail.com>
2023-04-18 00:31:19 +0900

    feat: [#76] 카카오 로그인 성공 후 nextjs로 note list 컴포넌트 리팩토링 시작

--- FE/front/src/component/userInfo.js
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-import { app } from "../util/realm";
-
-export default function UserInfo() {
-  const [user, setUser] = useState(app.currentUser);
-
-  return <div>userinfo : user id is.. {user.id}</div>;
-}
--- FE/front/src/util/realm.js
+++
@@ -1,2 +0,0 @@
-import * as Realm from "realm-web";
-export const app = new Realm.App({ id: "note-ppfia" });
