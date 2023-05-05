679279ae718bf98793183f33312f8ddfc54ca43b
onlinekkabchi <onlinekkabchi@gmail.com>
2023-05-05 16:06:50 +0900

    [refactor] react-router-dom으로 routing

--- FE/front/src/App.tsx
+++ FE/front/src/App.tsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import LoginEmail from "./components/LoginEmail";
+import { app } from "./util/realm";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState(app.currentUser);
 
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>아무단어장</h1>
+      {user ? (
+        <button
+          onClick={() => {
+            console.log(user);
+          }}
+        >
+          user
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      ) : (
+        <LoginEmail />
+      )}
+      <Outlet />
     </>
   );
 }
--- FE/front/src/main.tsx
+++ FE/front/src/main.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
