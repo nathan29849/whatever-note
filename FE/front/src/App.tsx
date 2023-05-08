da779b92ab885903a680bd7b6d830ff176261953
onlinekkabchi <onlinekkabchi@gmail.com>
2023-05-09 01:06:59 +0900

    updated

--- FE/front/src/App.tsx
+++ FE/front/src/App.tsx
@@ -1,35 +1,31 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-// import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
-// import { app } from "./util/realm";
+import { app } from "./util/realm";
 
-// import IndexMenu from "./components/IndexMenu";
-// import LoginEmail from "./components/LoginEmail";
-
-// function App() {
-//   const [user, setUser] = useState(null);
-//   const storage = app.currentUser;
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     if (storage !== null && storage.hasOwnProperty("_accessToken")) {
-//       setUser(storage);
-//       navigate("/notelist");
-//     }
-//   }, []);
-
-//   return (
-//     <div className="app">
-//       <p>아무단어장 리팩토링</p>
-//       <IndexMenu />
-//       {user ? <Outlet /> : <LoginEmail />}
-//     </div>
-//   );
-// }
+import IndexMenu from "./components/IndexMenu";
+import LoginEmail from "./components/LoginEmail";
 
 function App() {
-  return <div>vite server test</div>;
+  const [user, setUser] = useState(null);
+  const storage = app.currentUser;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (storage !== null && storage.hasOwnProperty("_accessToken")) {
+      setUser(storage);
+      navigate("/notelist");
+    }
+  }, []);
+
+  return (
+    <div className="app">
+      <p>아무단어장 리팩토링</p>
+      <IndexMenu />
+      {user ? <Outlet /> : <LoginEmail />}
+    </div>
+  );
 }
 
 export { App };
